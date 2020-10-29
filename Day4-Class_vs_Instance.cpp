5b8bdcdc40387ef20133a438633220455cfd0682
Sourabh Choudhary <64469149+sourabhskc@users.noreply.github.com>
2020-10-29 21:43:50 +0530

    Update Day4-Class_vs_Instance.cpp

--- Day4-Class_vs_Instance.cpp
+++ Day4-Class_vs_Instance.cpp
@@ -43,3 +43,20 @@ class Person{
     }
 
 int main(){
+    int t;
+	int age;
+    cin >> t;
+    for(int i=0; i < t; i++) {
+    	cin >> age;
+        Person p(age);
+        p.amIOld();
+        for(int j=0; j < 3; j++) {
+        	p.yearPasses(); 
+        }
+        p.amIOld();
+      
+		cout << '\n';
+    }
+
+    return 0;
+}
